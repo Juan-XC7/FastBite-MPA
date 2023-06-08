a0242424e660a21678df5ee97972b42a6695bb71
Juan Hernandez <20301061550317@cetis155.edu.mx>
2023-06-08 02:31:42 -0600

    login password validation

---
+++ public/js/redirect.js
@@ -0,0 +1,7 @@
+function homeRedirect(req, res) {
+    window.location.href = "/";
+}
+
+module.exports = {
+    homeRedirect,
+}
--- src/controllers/authentication-controller.js
+++ src/controllers/authentication-controller.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+const loginErrors = ""
+
 function login(req, res) {
     res.render('authentication/login.hbs');
 }
@@ -6,7 +9,39 @@ function register(req, res) {
     res.render('authentication/register.hbs');
 }
 
+function storeUser(req, res) {
+    const data = req.body;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    const isValidPassword = passwordRegex .test(data.password);
+    
+    if (isValidPassword !== true) {
+        const loginErrors = "invalid password";
+        console.log(loginErrors)
+        res.render('authentication/login.hbs', { loginErrors });
+        return false;
+    }
+
+    console.log('valid password');
+
+    if (data.password !== data.passwordConfirmation) {
+        const loginErrors = "Provided passwords don't match";
+        console.log(loginErrors)
+        res.render('authentication/login.hbs', { loginErrors });
+        return false;
+    }
+
+    bcrypt.hash(data.password, 12).then(hash => {
+        data.password = hash;
+        delete data.passwordConfirmation; // Delete the passwordConfirmation property
+        console.log(data);
+        res.render('home.hbs');
+    });
+
+}
+
 module.exports = {
-    login: login,
-    register: register,
+    login,
+    register,
+    storeUser,
+    loginErrors,
 }
